c1162ab01f7b36be9d60f65a3c730f6937c07261
Andrew Burgess <andrew@deceptacle.com>
2012-04-05 16:57:59 -0400

    Removed header comments

--- c-sharp/ProjectEuler/Problems/Problem001.cs
+++ c-sharp/ProjectEuler/Problems/Problem001.cs
@@ -4,15 +4,6 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler.Problems
 {
-    /*
-     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-     * The sum of these multiples is 23.
-     * 
-     * Find the sum of all the multiples of 3 or 5 below 1000.
-     * 
-     * Answer: 233168
-     * Execution Time: 0.002 seconds
-     */
     public class Problem001 : Problem
     {
         public override string Name { get { return "Add all the natural numbers below one thousand that are multiples of 3 or 5."; } }
